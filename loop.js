9de8f5d0b49ca74c8a7ea1b35c181665f05446c9
Craig Dejean <thelegendarycraig@yahoo.com>
2017-11-03 17:57:17 -0400

    test

---
+++ loop.js
@@ -0,0 +1,15 @@
+// for(var i = 0; i < 6; ++i) {
+//     console.log(i);
+// }
+
+var x = 3;
+var y = 1.1;
+// console.log(x + y);
+// console.log(x * y);
+// console.log((x + y) * (x - y));
+var z = x * y;
+// console.log(Math. sqrt(z));
+
+// console.log(Math.abs(y/x));
+
+console.log(z.toFixed(2));
