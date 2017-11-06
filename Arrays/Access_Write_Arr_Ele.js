98ae51222232c1ca2e4a22e80a7ff1f166ce44ca
Craig Dejean <thelegendarycraig@yahoo.com>
2017-11-06 14:44:19 -0500

    Accessing & writing array elements

---
+++ Arrays/Access_Write_Arr_Ele.js
@@ -0,0 +1,15 @@
+// Asigns 1 - 100 to the array
+
+// var nums = [];
+// for (var i = 0; i < 100; ++i) {
+//     nums[i] = i + 1;
+// }
+
+// console.log(nums);
+
+
+
+
+var numbers = [1,2,3,4,5];
+var sum = numbers[0] + numbers[1] + numbers[2] + numbers[3] + numbers[4];
+console.log(sum);
